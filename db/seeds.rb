7b412d378d6d0da936c635c0757787f943c14355
Noam Lubofsky <noamlubofsky@gmail.com>
2021-07-26 16:01:25 -0400

    commit tree 58b56e24024f29c628fbd7ab4d6f5a942bfc2f42

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,17 @@ Student.create(name: "April", grade: "10th")
 Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
+
+
+
+
+  desc 'drop into the Pry console'
+task console: :environment do
+  Pry.start
+end 
+
+
+task :environment do
+    require_relative './config/environment'
+  end 
+  
